68af7e192b53432099899a71a173185865a1413b
jayden-lee <gwanggi.lee@gmail.com>
2019-01-14 16:15:34 +0900

    add Transaction Commit and Rollback

---
+++ src/main/java/com/jayden/study/transaction/CommitAndRollback.java
@@ -0,0 +1,53 @@
+package com.jayden.study.transaction;
+
+import com.jayden.study.utils.JdbcUtils;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+/**
+ * JDBC Commit and Rollback
+ *
+ * @author jayden-lee
+ */
+public class CommitAndRollback {
+    private static final String url = "jdbc:mysql://localhost:3306/mysql";
+    private static final String user = "user";
+    private static final String password = "password";
+
+    private static Connection connection;
+
+    public static void main(String[] args) {
+        try {
+            connection = JdbcUtils.getConnection(url, user, password);
+
+            // change auto commit status
+            connection.setAutoCommit(false);
+
+            // Execute update Query
+            updateQuery();
+
+            // Commit
+            connection.commit();
+        } catch (Exception e) {
+            try {
+                // Rollback
+                connection.rollback();
+            } catch (SQLException e1) {
+                e1.printStackTrace();
+            }
+            e.printStackTrace();
+        }
+    }
+
+    private static void updateQuery() throws SQLException {
+        String sql = "INSERT INTO testdb.employee VALUES (150, 'jayden-lee', 'IT', 4)";
+
+        try (Statement statement = connection.createStatement()) {
+            statement.executeUpdate(sql);
+        } catch (SQLException e) {
+            throw e;
+        }
+    }
+}
