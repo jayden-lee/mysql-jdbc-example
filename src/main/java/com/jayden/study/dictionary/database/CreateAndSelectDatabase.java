9215bf6e409be7a4aaa9a880c9e5c669172d2ad9
jayden-lee <gwanggi.lee@gmail.com>
2019-01-18 14:24:09 +0900

    add Creating and Selecting a Database

---
+++ src/main/java/com/jayden/study/dictionary/database/CreateAndSelectDatabase.java
@@ -0,0 +1,79 @@
+package com.jayden.study.dictionary.database;
+
+import com.jayden.study.utils.JdbcUtils;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+/**
+ * Creating and Selecting a Database
+ *
+ * @author jayden-lee
+ */
+public class CreateAndSelectDatabase {
+    private static final String url = "jdbc:mysql://localhost:3306/mysql";
+    private static final String user = "user";
+    private static final String password = "password";
+
+    private static Connection connection;
+
+    private static Type type = Type.JDBC_API;
+
+    public enum Type {
+        JDBC_API,
+        SQL
+    }
+
+    public static void main(String[] args) {
+        try {
+            connection = JdbcUtils.getConnection(url, user, password);
+
+            printCurrentDatabase();
+
+            createDatabase();
+
+            selectDatabase();
+
+            printCurrentDatabase();
+        } catch (Exception e) {
+            e.printStackTrace();
+
+        } finally {
+            JdbcUtils.closeConnection(connection);
+        }
+    }
+
+    private static void createDatabase() throws SQLException {
+        System.out.println("Create a testdb2 Database");
+
+        String sql = "CREATE DATABASE testdb2";
+
+        try (Statement statement = connection.createStatement()) {
+            statement.execute(sql);
+        } catch (SQLException e) {
+            throw e;
+        }
+    }
+
+    private static void selectDatabase() throws SQLException {
+        System.out.println("Select a testdb2 Database");
+
+        if (Type.JDBC_API == type) {
+            connection.setCatalog("testdb2");
+
+        } else if (Type.SQL == type) {
+            String sql = "USE testdb2";
+
+            try (Statement statement = connection.createStatement()) {
+                statement.execute(sql);
+            } catch (SQLException e) {
+                throw e;
+            }
+        }
+    }
+
+    private static void printCurrentDatabase() throws SQLException {
+        System.out.println("Current Database: " + connection.getCatalog());
+    }
+}
---
+++ src/main/java/com/jayden/study/dictionary/database/README.md
@@ -0,0 +1,19 @@
+# Database
+
+# 1. Creating and Selecting a Database
+
+## 1.1 Create a Database
+```
+CREATE DATABASE testdb;
+```
+
+## 1.2 Select a Database
+```
+USE testdb;
+```
+
+or
+
+```
+connection.setSchema("testdb");
+```
